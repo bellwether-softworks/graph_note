607244a321d6d9010af4725f2fb843ae8882edd7
Justin Bell <justin.bell@bellwether-softworks.com>
2024-08-15 09:20:19 -0500

    Barebones egui app

---
+++ src/app.rs
@@ -0,0 +1,26 @@
+use eframe::Frame;
+use egui::Context;
+
+pub struct App;
+
+impl Default for App {
+    fn default() -> Self {
+        Self
+    }
+}
+
+impl App {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Access to the CreationContext during instantiation allows us to take
+        // ownership of some aspects of our app, such as wiring up fonts.
+        Default::default()
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Greetings from the skeleton app.");
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    let native_options = eframe::NativeOptions::default();
+
+    eframe::run_native(
+        "Graph Notes",
+        native_options,
+        Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
+    )
 }
