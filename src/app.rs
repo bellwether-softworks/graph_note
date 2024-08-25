bb9669c098191f2e4bcf7891ef32e4c8f6a7ffd0
Justin Bell <justin.bell@bellwether-softworks.com>
2024-08-25 09:06:06 -0500

    Adding title and timestamp to notes, adding list for notes, allowing for CRUD operations on the list of notes

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,34 @@
+use std::time::SystemTime;
 use eframe::Frame;
-use egui::Context;
+use egui::{Context, Grid};
+use time::OffsetDateTime;
+
+struct Note {
+    created_on: SystemTime,
+    title: String,
+    text: String,
+}
+
+impl Note {
+    pub fn new() -> Self {
+        Note {
+            created_on: SystemTime::now(),
+            title: String::new(),
+            text: String::new(),
+        }
+    }
+}
 
 pub struct App {
-    note: String,
+    selected_note: usize,
+    notes: Vec<Note>,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            note: String::new()
+            notes: vec![Note::new()],
+            selected_note: 0,
         }
     }
 }
@@ -24,9 +44,72 @@ impl App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Note text");
+            if ui.button("Add new note").clicked() {
+                self.notes.push(Note::new());
+                self.selected_note = self.notes.len() - 1;
+            }
 
-            egui::TextEdit::multiline(&mut self.note).show(ui);
+            let mut to_remove = None;
+            let mut to_select = None;
+
+            Grid::new("Notes")
+                .show(ui, |ui| {
+                    // Header row
+                    ui.label("");
+                    ui.label("Date");
+                    ui.label("Title");
+                    ui.end_row();
+
+                    for (index, note) in self.notes.iter().enumerate() {
+                        ui.label(if index == self.selected_note { "*" } else { "" });
+                        ui.label(OffsetDateTime::from(note.created_on).to_string());
+                        ui.label(note.title.as_str());
+                        if ui.button("Edit").clicked() {
+                            to_select = Some(index);
+                        }
+                        if ui.button("Delete").clicked() {
+                            if self.notes.len() == 1 {
+                                // This is the last note; at this point, our selection index is meaningless,
+                                // but can't dip below 0, so we'll just ignore it for now
+                                to_remove = Some(index);
+                            } else if index == self.selected_note && index == self.notes.len() - 1 {
+                                // This is the note at the end of the list; let's decrement and display
+                                // the last available note
+                                to_remove = Some(index);
+                                to_select = Some(self.selected_note - 1);
+                            } else if index < self.selected_note {
+                                // Removing a note before our current selection, so we need to move
+                                // with it accordingly
+                                to_remove = Some(index);
+                                to_select = Some(self.selected_note - 1);
+                            } else {
+                                to_remove = Some(index);
+                            }
+                        }
+                        ui.end_row();
+                    }
+                });
+
+            if let Some(to_select) = to_select {
+                self.selected_note = to_select;
+            }
+            if let Some(to_remove) = to_remove {
+                self.notes.remove(to_remove);
+            }
+
+            ui.separator();
+
+            if let Some(mut selected_note) = self.notes.get_mut(self.selected_note) {
+                ui.horizontal(|ui| {
+                    ui.label("Title");
+                    ui.text_edit_singleline(&mut selected_note.title);
+                });
+
+                ui.label(format!("Created at {:?}", OffsetDateTime::from(selected_note.created_on)));
+
+                ui.label("Text");
+                egui::TextEdit::multiline(&mut selected_note.text).show(ui);
+            }
         });
     }
 }
